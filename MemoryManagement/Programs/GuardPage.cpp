79c20b98411be30e753e473be3fd4f9e27fafe14
kyledotterrer <kdotterrer@gmail.com>
2020-01-19 13:42:36 -0500

    more memory management updates

--- MemoryManagement/Programs/GuardPage.cpp
+++ MemoryManagement/Programs/GuardPage.cpp
@@ -1,27 +1,30 @@
 // GuardPage.cpp
 // Demo of utilizing virtual memory API to create a guard page.
+//
+// Build:
+//  cl /EHsc /nologo GuardPage.cpp
 
 #define UNICODE
 #define _UNICODE
 
 #include <windows.h>
-
 #include <cstdio>
+
+constexpr auto STATUS_SUCCESS_I = 0x0;
+constexpr auto STATUS_FAILURE_I = 0x1;
 
 INT main(VOID)
 {
-    puts("Hello Guard Page!");
-
     SYSTEM_INFO info;
-    GetSystemInfo(&info);
+    ::GetSystemInfo(&info);
 
-    auto dwPageSize = info.dwPageSize;
+    auto dwPageSize              = info.dwPageSize;
     auto dwAllocationGranularity = info.dwAllocationGranularity;
 
-    printf("Page Size: %u\n", dwPageSize);
-    printf("Allocation Granularity: %u\n", dwAllocationGranularity);
+    printf("[+] Page Size: %u\n", dwPageSize);
+    printf("[+] Allocation Granularity: %u\n", dwAllocationGranularity);
 
-    LPVOID ptr = VirtualAlloc(
+    LPVOID ptr = ::VirtualAlloc(
         nullptr, 
         dwPageSize, 
         MEM_COMMIT | MEM_RESERVE, 
@@ -29,43 +32,47 @@ INT main(VOID)
         );
     if (NULL == ptr)
     {
-        printf("Failed to allocate page (VirtualAlloc())\n");
-        printf("GLE: %u\n", GetLastError());
-        return 1;
+        printf("[-] Failed to allocate page (VirtualAlloc())\n");
+        printf("[-] GLE: %u\n", ::GetLastError());
+        return STATUS_FAILURE_I;
     }
 
-    printf("Successfully allocated page\n");
+    printf("[+] Successfully allocated page\n");
 
     // TODO: determine why this example did not work with SEH mechanisms;
     // e.g. when I wrapped this VirtualLock() call in an SEH guarded
     // region and had the filter expression test for STATUS_GUARD_PAGE_VIOLATION
     // the handler was never invoked, and the page was successfully locked(?)
+    //
+    // Working Theory: the exception is swallowed by the virtual memory
+    // API and is reflected to the user-level API by the return value
+    // of the VirtualLock() call
 
     // attempt to lock the page
-    BOOL bLocked = VirtualLock(ptr, dwPageSize);
+    BOOL bLocked = ::VirtualLock(ptr, dwPageSize);
     if (!bLocked)
     {
-        printf("Failed to lock page on first attempt...\n");
+        printf("[+] Failed to lock page on first attempt...\n");
     }
     else
     {
-        printf("Successfully locked page on first attempt\n");
-        VirtualUnlock(ptr, dwPageSize);
+        printf("[+] Successfully locked page on first attempt\n");
+        ::VirtualUnlock(ptr, dwPageSize);
     }
 
-    bLocked = VirtualLock(ptr, dwPageSize);
+    bLocked = ::VirtualLock(ptr, dwPageSize);
     if (!bLocked)
     {
-        printf("Failed to lock page on second attempt...\n");
+        printf("[+] Failed to lock page on second attempt...\n");
     }
     else
     {
-        printf("Successfully locked page on second attempt\n");
-        VirtualUnlock(ptr, dwPageSize);
+        printf("[+] Successfully locked page on second attempt\n");
+        ::VirtualUnlock(ptr, dwPageSize);
     }
 
     // dwSize must be 0 for use with MEM_RELEASE 
-    VirtualFree(ptr, 0, MEM_RELEASE);
+    ::VirtualFree(ptr, 0, MEM_RELEASE);
 
-    return 0;
+    return STATUS_SUCCESS_I;
 }
--- MemoryManagement/Programs/HeapWalk.cpp
+++ MemoryManagement/Programs/HeapWalk.cpp
@@ -1,5 +1,8 @@
 // HeapWalk.cpp
 // Demo of manually walking the allocations within a user-created heap.
+//
+// Build:
+//  cl /EHsc /nologo HeapWalk.cpp
 
 #define UNICODE
 #define _UNICODE
@@ -8,35 +11,56 @@
 #include <tchar.h>
 #include <stdio.h>
 
-int __cdecl _tmain()
+constexpr auto STATUS_SUCCESS_I = 0x0;
+constexpr auto STATUS_FAILURE_I = 0x1;
+
+VOID WalkHeap(HANDLE hHeap);
+
+INT _tmain()
 {
-    DWORD LastError;
-    HANDLE hHeap;
-    PROCESS_HEAP_ENTRY Entry;
+    HANDLE hCustomHeap;
 
     // create a new heap with default parameters
-    hHeap = HeapCreate(0, 0, 0);
-    if (hHeap == NULL) 
+    hCustomHeap = ::HeapCreate(0, 0, 0);
+    if (hCustomHeap == NULL) 
     {
         _tprintf(TEXT("Failed to create a new heap with LastError %d.\n"),
-                 GetLastError());
-        return 1;
+                 ::GetLastError());
+        return STATUS_FAILURE_I;
     }
 
-    hHeap = GetProcessHeap();
+    // walk the custom and default process heaps
+    WalkHeap(::GetProcessHeap());
+    WalkHeap(hCustomHeap);
+
+    // destroy the custom heap
+    if (::HeapDestroy(hCustomHeap) == FALSE) 
+    {
+        _tprintf(TEXT("Failed to destroy heap with LastError %d.\n"),
+                 ::GetLastError());
+    }
+
+    return STATUS_SUCCESS_I;
+}
+
+VOID WalkHeap(HANDLE hHeap)
+{
+    DWORD              LastError;
+    PROCESS_HEAP_ENTRY Entry;
 
     // lock the heap to prevent other threads from accessing the heap during enumeration
-    if (HeapLock(hHeap) == FALSE) 
+    // not actually necessary in this single-threaded application, but illustrative
+    if (::HeapLock(hHeap) == FALSE) 
     {
         _tprintf(TEXT("Failed to lock heap with LastError %d.\n"),
-                 GetLastError());
-        return 1;
+                 ::GetLastError());
+        return;
     }
 
     _tprintf(TEXT("Walking heap %#p...\n\n"), hHeap);
 
     Entry.lpData = NULL;
-    while (HeapWalk(hHeap, &Entry) != FALSE) 
+    while (::HeapWalk(hHeap, &Entry) != FALSE) 
     {
         if ((Entry.wFlags & PROCESS_HEAP_ENTRY_BUSY) != 0) 
         {
@@ -79,24 +103,15 @@ int __cdecl _tmain()
                  Entry.iRegionIndex);
     }
 
-    LastError = GetLastError();
+    LastError = ::GetLastError();
     if (LastError != ERROR_NO_MORE_ITEMS) 
     {
         _tprintf(TEXT("HeapWalk failed with LastError %d.\n"), LastError);
     }
 
     // unlock the heap to allow other threads to access the heap after enumeration has completed
-    if (HeapUnlock(hHeap) == FALSE) {
+    if (::HeapUnlock(hHeap) == FALSE) {
         _tprintf(TEXT("Failed to unlock heap with LastError %d.\n"),
-                 GetLastError());
+                 ::GetLastError());
     }
-
-    // destroy the heap
-    if (HeapDestroy(hHeap) == FALSE) 
-    {
-        _tprintf(TEXT("Failed to destroy heap with LastError %d.\n"),
-                 GetLastError());
-    }
-
-    return 0;
 }
