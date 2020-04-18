35882819783cfa8a13b5edc54fda62cd779d93be
kyledotterrer <kdotterrer@gmail.com>
2020-04-18 11:57:05 -0400

    init once initialization

--- Synchronization/init_once.cpp
+++ Synchronization/init_once.cpp
@@ -9,3 +9,48 @@
 #include <catch.hpp>
 
 #include <windows.h>
+
+#include <thread>
+#include <vector>
+
+constexpr const auto N_THREADS = 5;
+
+struct Data
+{
+    unsigned long long n;
+};
+
+INIT_ONCE g_init = INIT_ONCE_STATIC_INIT;
+
+BOOL __stdcall invoked_once(
+    PINIT_ONCE /* init_once */,
+    void*      param,
+    void**     /* context */
+    )
+{
+    auto& data = *static_cast<Data*>(param);
+    ++data.n;
+
+    return TRUE;
+}
+
+TEST_CASE("Function provided to InitOnceExecuteOnce() is indeed only executed once")
+{
+    auto data = Data{0};
+
+    auto threads = std::vector<std::thread>{};
+    for (auto i = 0u; i < N_THREADS; ++i)
+    {
+        threads.emplace_back([&data]()
+        {
+            ::InitOnceExecuteOnce(&g_init, invoked_once, &data, nullptr);
+        });
+    }
+
+    for (auto& t : threads)
+    {
+        t.join();
+    }
+
+    REQUIRE(data.n == 1);
+}
